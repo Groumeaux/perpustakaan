a20974f86d5f331ea379607971592c8cb983051b
Rikkii1049 <rickymakalalag2013@gmail.com>
2024-12-14 23:06:38 +0800

    pls work

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,17 +4,17 @@
 service mysql start
 
 # Initialize the database if it doesn't already exist
-if [ ! -d "/var/lib/mysql/mydb" ]; then
+if [ ! -d "/var/lib/mysql/data_perpus" ]; then
     echo "Initializing MySQL database..."
-    mysql -e "CREATE DATABASE mydb;"
-    mysql -e "CREATE USER 'user'@'%' IDENTIFIED BY 'password';"
-    mysql -e "GRANT ALL PRIVILEGES ON mydb.* TO 'user'@'%';"
+    mysql -e "CREATE DATABASE data_perpus;"
+    mysql -e "CREATE USER 'root'@'%' IDENTIFIED BY 'passworddb';"
+    mysql -e "GRANT ALL PRIVILEGES ON data_perpus.* TO 'root'@'%';"
     mysql -e "FLUSH PRIVILEGES;"
     
     # Import the SQL file into the database
     if [ -f /docker-entrypoint-initdb.d/data_perpus.sql ]; then
         echo "Importing database from db_export.sql..."
-        mysql -u root mydb < /docker-entrypoint-initdb.d/data_perpus.sql
+        mysql -u root data_perpus < /docker-entrypoint-initdb.d/data_perpus.sql
     else
         echo "No SQL dump file found to import."
     fi
